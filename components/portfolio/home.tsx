6b1a02f5648da594de3575560d4e7375ca08d2b2
Noah Weingand <noahweingand@gmail.com>
2021-07-27 21:34:33 -0400

    center name/title on portfolio/home

--- components/portfolio/home.tsx
+++ components/portfolio/home.tsx
@@ -5,10 +5,11 @@ type HomeProps = {
 
 export const Home: React.FC<HomeProps> = ({ name, title }) => {
   return (
-    <div className="video-wrapper">
-      <h1 className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-6xl text-white font-semibold">
-        {name}
-      </h1>
+    <div>
+      <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 grid content-center justify-center items-center text-white">
+        <h1 className="flex m-16 text-6xl font-semibold">{name}</h1>
+        <h3 className="flex justify-center text-2xl">{title}</h3>
+      </div>
       {/* <h3 className="absolute inset-0 z-20 flex justify-center items-center text-4xl text-white">
         {title}
       </h3> */}
@@ -16,8 +17,8 @@ export const Home: React.FC<HomeProps> = ({ name, title }) => {
         autoPlay
         loop
         muted
-        //className="w-full"
-        className="background-video"
+        className="w-full"
+        //className="background-video"
       >
         <source
           // src="https://assets.mixkit.co/videos/preview/mixkit-set-of-plateaus-seen-from-the-heights-in-a-sunset-26070-large.mp4"
