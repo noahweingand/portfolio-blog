84aa2ac86ba002878a8edd780c6efb530d4da316
Noah Weingand <noahweingand@gmail.com>
2021-07-28 21:39:19 -0400

    portfolio home styling/organzing

--- components/portfolio-container.tsx
+++ components/portfolio-container.tsx
@@ -2,7 +2,21 @@ import { Nav, Home } from './portfolio/index';
 
 export const PortfolioContainer = () => {
   return (
-    <div className="">
+    <div className="relative h-screen">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute w-full"
+        //className="background-video"
+      >
+        <source
+          // src="https://assets.mixkit.co/videos/preview/mixkit-set-of-plateaus-seen-from-the-heights-in-a-sunset-26070-large.mp4"
+          src="/coding.mov"
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
       <Nav />
       <Home name="Noah Weingand" title="Full Stack Developer" />
     </div>
--- components/portfolio/home.tsx
+++ components/portfolio/home.tsx
@@ -5,7 +5,7 @@ type HomeProps = {
 
 export const Home: React.FC<HomeProps> = ({ name, title }) => {
   return (
-    <div>
+    <div className="">
       <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 grid content-center justify-center items-center text-white">
         <h1 className="flex m-16 text-6xl font-semibold">{name}</h1>
         <h3 className="flex justify-center text-2xl">{title}</h3>
@@ -13,20 +13,6 @@ export const Home: React.FC<HomeProps> = ({ name, title }) => {
       {/* <h3 className="absolute inset-0 z-20 flex justify-center items-center text-4xl text-white">
         {title}
       </h3> */}
-      <video
-        autoPlay
-        loop
-        muted
-        className="w-full"
-        //className="background-video"
-      >
-        <source
-          // src="https://assets.mixkit.co/videos/preview/mixkit-set-of-plateaus-seen-from-the-heights-in-a-sunset-26070-large.mp4"
-          src="/coding.mov"
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
     </div>
   );
 };
