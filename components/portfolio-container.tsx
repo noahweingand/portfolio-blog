716fea8442c66e7e3ae3a2c37a295d7fd68429d2
Noah Weingand <noahweingand@gmail.com>
2021-07-28 22:40:39 -0400

    finished home/nav/video stlying

--- components/portfolio-container.tsx
+++ components/portfolio-container.tsx
@@ -2,23 +2,13 @@ import { Nav, Home } from './portfolio/index';
 
 export const PortfolioContainer = () => {
   return (
-    <div className="relative h-screen">
-      <video
-        autoPlay
-        loop
-        muted
-        className="absolute w-full"
-        //className="background-video"
-      >
-        <source
-          // src="https://assets.mixkit.co/videos/preview/mixkit-set-of-plateaus-seen-from-the-heights-in-a-sunset-26070-large.mp4"
-          src="/coding.mov"
-          type="video/mp4"
-        />
+    <header className="relative h-screen overflow-hidden">
+      <video autoPlay loop muted className="absolute z-0 w-auto min-w-full min-h-full max-w-none">
+        <source src="/coding.mov" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <Nav />
       <Home name="Noah Weingand" title="Full Stack Developer" />
-    </div>
+    </header>
   );
 };
--- components/portfolio/home.tsx
+++ components/portfolio/home.tsx
@@ -5,14 +5,9 @@ type HomeProps = {
 
 export const Home: React.FC<HomeProps> = ({ name, title }) => {
   return (
-    <div className="">
-      <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 grid content-center justify-center items-center text-white">
-        <h1 className="flex m-16 text-6xl font-semibold">{name}</h1>
-        <h3 className="flex justify-center text-2xl">{title}</h3>
-      </div>
-      {/* <h3 className="absolute inset-0 z-20 flex justify-center items-center text-4xl text-white">
-        {title}
-      </h3> */}
+    <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 grid content-center justify-center items-center text-white">
+      <h1 className="flex m-16 text-6xl font-semibold">{name}</h1>
+      <h3 className="flex justify-center text-2xl">{title}</h3>
     </div>
   );
 };
