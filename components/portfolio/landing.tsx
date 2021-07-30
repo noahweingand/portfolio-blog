e3046085b0a3eec016055e17b771f74f97eaf1c4
Noah Weingand <noahweingand@gmail.com>
2021-07-29 20:29:43 -0400

    sheeeeshh styling hard

--- components/portfolio/landing.tsx
+++ components/portfolio/landing.tsx
@@ -5,8 +5,8 @@ type HomeProps = {
 
 export const Landing: React.FC<HomeProps> = ({ name, title }) => {
   return (
-    <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 grid content-center justify-center items-center text-white">
-      <h1 className="flex m-16 text-6xl font-semibold">{name}</h1>
+    <div className="relative mt-16 opacity-0 hover:opacity-100 duration-300 grid content-center justify-center items-center h-screen text-white">
+      <h1 className="flex mb-16 text-6xl font-semibold">{name}</h1>
       <h3 className="flex justify-center text-2xl">{title}</h3>
     </div>
   );
