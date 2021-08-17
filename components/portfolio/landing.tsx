c0a13dc21c241aacda369e27a2c88303b81d352d
Noah Weingand <noahweingand@gmail.com>
2021-08-16 23:16:13 -0400

    style spotify on landing

--- components/portfolio/landing.tsx
+++ components/portfolio/landing.tsx
@@ -9,9 +9,11 @@ type HomeProps = {
 
 export const Landing: React.FC<HomeProps> = ({ name, title, data }) => {
   return (
-    <div className="relative mt-16 opacity-0 hover:opacity-100 duration-300 grid content-center justify-center items-center h-screen text-white">
-      <h1 className="flex mb-16 text-6xl font-semibold">{name}</h1>
-      <h3 className="flex justify-center text-2xl">{title}</h3>
+    <div className="flex h-screen">
+      <div className="relative m-auto grid content-center justify-center items-center text-white">
+        <h1 className="flex mb-16 text-6xl font-semibold">{name}</h1>
+        <h3 className="flex justify-center text-2xl">{title}</h3>
+      </div>
       <SpotifyStub isPlaying={data.isPlaying} item={data.item} />
     </div>
   );
--- components/portfolio/spotify-stub.tsx
+++ components/portfolio/spotify-stub.tsx
@@ -1,10 +1,61 @@
 import React from 'react';
+import { SiSpotify } from 'react-icons/si';
+import { IconType } from 'react-icons';
+import 'react-tippy/dist/tippy.css';
+import { Tooltip } from 'react-tippy';
 import { SpotifyItem } from '../../types/spotify';
 
-export const SpotifyStub: React.FC<SpotifyItem> = ({ isPlaying, item }) => {
+const renderArtists = (artists: string[]): string => {
+  let artistNames = '';
+  let artistNamesLength = artists.length;
+  artists.forEach((artist, idx) => {
+    if (idx === artistNamesLength - 1) {
+      artistNames = artistNames + artist;
+    } else {
+      artistNames = artistNames + `${artist}, `;
+    }
+  });
+  return artistNames;
+};
+
+const Icon = ({ icon, title }: { icon: IconType; title: string }) => {
   return (
-    <div className="content-end">
-      <div className="content-end">test</div>
+    <Tooltip title={title}>
+      {icon({ className: 'pb-1 inline', size: '2em', fill: 'green' })}
+    </Tooltip>
+  );
+};
+
+export const SpotifyStub: React.FC<SpotifyItem> = ({ isPlaying, item }) => {
+  const icon = isPlaying ? (
+    <span className="bg-green-500 h-2 w-2 animate-pulse mr-2 rounded-full inline-block"></span>
+  ) : (
+    <span className="bg-red-500 h-2 w-2 animate-pulse mr-2 rounded-full inline-block"></span>
+  );
+  return (
+    <div className="absolute bottom-0 right-0">
+      <div className="bottom-0 w-full text-white content-end">
+        <span>
+          {icon}
+          <a
+            className="hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={item.externalUrl}
+          >
+            <span className="font-bold">{item.name}</span>
+          </a>{' '}
+          by{' '}
+          <span className="font-bold">
+            {renderArtists(item.artists)} {'    '}
+          </span>
+          <span>
+            <a target="_blank" rel="noopener noreferrer" href={item.profileUrl}>
+              <Icon icon={SiSpotify} title={'Spotify'} />
+            </a>
+          </span>
+        </span>
+      </div>
     </div>
   );
 };
