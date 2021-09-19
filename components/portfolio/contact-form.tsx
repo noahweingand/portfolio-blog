91f9b6cf643a757475ba884b36d0c496ab9dbe33
Noah Weingand <noahweingand@gmail.com>
2021-09-19 00:59:59 -0400

    main div mobile padding

--- components/portfolio/contact-form.tsx
+++ components/portfolio/contact-form.tsx
@@ -58,7 +58,7 @@ export const ContactForm = () => {
   };
 
   return (
-    <form className="w-full max-w-lg" onSubmit={onSubmit}>
+    <form className="w-full max-w-lg px-4" onSubmit={onSubmit}>
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2">
