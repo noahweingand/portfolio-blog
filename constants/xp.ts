98118b6ab2d06a648739955e207bd4463cfa8226
Noah Weingand <noahweingand@gmail.com>
2021-09-18 22:33:57 -0400

    hardcode content for now

---
+++ constants/xp.ts
@@ -0,0 +1,67 @@
+export const xp = [
+  {
+    company: 'CoStar Group, Inc.',
+    title: 'Associate Software Engineer',
+    imagePath: '/experience/costar_logo.png',
+    altImageText: 'CoStar Group, Inc. logo',
+    startDate: 'June 2021',
+    endDate: 'Present',
+    status: 'Full-time',
+    location: 'Richmond, VA',
+    description: [
+      `According to a study, it has been proven that there is a tight bond between cats and
+      humans. According to a study, it has been proven that there is a tight bond between cats
+      and humans.`,
+      `According to a study, it has been proven that there is a tight bond between cats and
+      humans. According to a study, it has been proven that there is a tight bond between cats
+      and humans.`,
+    ],
+  },
+  {
+    company: 'Tahzoo',
+    title: 'Associate Developer',
+    imagePath: '/experience/tahzoo_logo.png',
+    altImageText: 'Tahzoo logo',
+    startDate: 'August 2020',
+    endDate: 'June 2021',
+    status: 'Full-time',
+    location: 'Richmond, VA',
+    description: [
+      `According to a study, it has been proven that there is a tight bond between cats and
+      humans. According to a study, it has been proven that there is a tight bond between cats
+      and humans.`,
+      `According to a study, it has been proven that there is a tight bond between cats and
+      humans. According to a study, it has been proven that there is a tight bond between cats
+      and humans.`,
+    ],
+  },
+  {
+    company: 'Tahzoo',
+    title: 'Technical Intern',
+    imagePath: '/experience/tahzoo_logo.png',
+    altImageText: 'Tahzoo logo',
+    startDate: 'December 2020',
+    endDate: 'August 2020',
+    status: 'Part-time',
+    location: 'Richmond, VA',
+    description: [
+      `According to a study, it has been proven that there is a tight bond between cats and
+      humans. According to a study, it has been proven that there is a tight bond between cats
+      and humans.`,
+      `According to a study, it has been proven that there is a tight bond between cats and
+      humans. According to a study, it has been proven that there is a tight bond between cats
+      and humans.`,
+    ],
+  },
+  {
+    company: 'Virginia Commonwealth University',
+    title: 'Student',
+    imagePath: '/experience/vcu.png',
+    altImageText: 'Virginia Commonwealth University logo',
+    startDate: 'August 2016',
+    endDate: 'May 2021',
+    status: 'Full-time',
+    location: 'Richmond, VA',
+    description: [`Bachelor of Science in Computer Science`, `Minor in Mathematics`],
+  },
+];
