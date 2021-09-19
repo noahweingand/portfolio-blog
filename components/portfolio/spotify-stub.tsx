e79d43dd58e616d1b0d9aa356c6b4db48bbc78f5
Noah Weingand <noahweingand@gmail.com>
2021-09-19 17:16:03 -0400

    hide artists on smaller screens

--- components/portfolio/spotify-stub.tsx
+++ components/portfolio/spotify-stub.tsx
@@ -26,29 +26,31 @@ const renderArtists = (artists: string[], songName: string): string => {
 };
 
 export const SpotifyStub: React.FC<SpotifyItem> = ({ item, isPlaying }) => {
-  const icon = isPlaying ? (
-    <span className="bg-green-500 h-2 w-2 animate-pulse mr-2 mb-0.5 rounded-full inline-block"></span>
+  const isPlayingIcon = isPlaying ? (
+    <span className="bg-green-500 h-2 w-2 animate-pulse mt-2.5 mr-2.5 mb-0.5 rounded-full inline-block"></span>
   ) : (
-    <span className="bg-red-500 h-2 w-2 animate-pulse mr-2 mb-0.5 rounded-full inline-block"></span>
+    <span className="bg-red-500 h-2 w-2 animate-pulse mt-2.5 mr-2.5 mb-0.5 rounded-full inline-block"></span>
   );
 
   return (
     <div className="absolute bottom-2 right-2 content-end rounded-xl bg-opacity-75 bg-gray-800 pt-2 px-4">
       <div className="flex bottom-0 w-full text-white text-lg justify-around">
-        <span className="">
-          {icon}
+        <span className="flex">
+          {isPlayingIcon}
           <a
             className="hover:underline"
             target="_blank"
             rel="noopener noreferrer"
             href={item.externalUrl}
           >
-            <span className="font-bold">{item.name}</span>
+            <span className="font-bold pr-1">{item.name}</span>
           </a>
-          {'   '}
-          by {'   '}
-          <span className="font-bold">
-            {renderArtists(item.artists, item.name)} {'    '}
+          <span className="hidden md:block">
+            {'   '}
+            by {'   '}
+            <span className="font-bold">
+              {renderArtists(item.artists, item.name)} {'    '}
+            </span>
           </span>
         </span>
         <span className="pl-2 justify-items-end">
